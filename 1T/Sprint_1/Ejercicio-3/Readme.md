60849502786400672ffe1f4322a21eeabc8c45ed
Carlos Marban <152007178+zencare4k@users.noreply.github.com>
2024-10-02 12:07:53 +0200

    update

--- 1T/Sprint_1/Ejercicio-3/Readme.md
+++ 1T/Sprint_1/Ejercicio-3/Readme.md
@@ -1,13 +1,11 @@
 # Ejercicio 3
-## Análisis del problema
+
+## Análisis del problema 
 
 - Crea una página con un campo de entrada y un botón etiquetado "Añadir a la lista".
-
 - También debes tener una lista vacía (`<ul>` o `<ol>`).
-
 - Cuando el usuario escribe algo en el campo de entrada y hace clic en el botón, entonces el contenido del campo debe agregarse como un nuevo ítem (`<li>`) a la lista.
 
-- Pista: Utiliza el método `.createElement()` y `.appendChild()` del DOM.
+## Prueba de la resolución del problema
 
-## Pruebas de la resolución del problema
-
+![imagen3](ejercicio3.gif)
--- 1T/Sprint_1/Ejercicio-4/ejercicio4.js
+++ 1T/Sprint_1/Ejercicio-4/ejercicio4.js
@@ -4,16 +4,22 @@ function dinamicstyle() {
 
     div1.addEventListener('mouseover', function() {
         div1.style.backgroundColor = 'blue';
+        div1.style.color = 'white';
     });
     div1.addEventListener('mouseout', function() {
         div1.style.backgroundColor = 'white';
+        div1.style.color = 'black';
+
     });
 
     div2.addEventListener('mouseover', function() {
         div2.style.backgroundColor = 'blue';
+        div2.style.color = 'white';
+
     });
     div2.addEventListener('mouseout', function() {
         div2.style.backgroundColor = 'white';
+        div2.style.color = 'black';
     });
 }
 dinamicstyle();
