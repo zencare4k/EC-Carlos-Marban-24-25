673764a8d60b7055f1d390b6b4aee6c270fefd27
z4k23 <122319845+z4k23@users.noreply.github.com>
2024-10-17 19:13:13 +0200

    xd

--- 1T/Opcionales/ejercicio1/ejercicio1/src/App.js
+++ 1T/Opcionales/ejercicio1/ejercicio1/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Component from './Component'
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <Component/>
       </header>
     </div>
   );
---
+++ 1T/Opcionales/ejercicio1/ejercicio1/src/Component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from './Component.module.css'
+export default function DiscordLogin() {
+    
+    return(
+        
+        <div>
+            <a href="https://discord.com/oauth2/authorize?client_id=1296384553379827712" target="_blank" className="DiscordButton"><img src="https://i.imgur.com/RBfcoBt.png" alt="" id="DsicordImage" /></a>
+        </div>
+    )
+}
