15aef5d8dc74c8ca2dec3546afd989c9d6894958
z4k23 <122319845+z4k23@users.noreply.github.com>
2024-10-18 22:37:47 +0200

    xd

--- 1T/Opcionales/ejercicio1/ejercicio1/src/App.js
+++ 1T/Opcionales/ejercicio1/ejercicio1/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Component from './Component'
 function App() {
   return (
-    <div className="App">
+    <div className="App">3
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
