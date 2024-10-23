9ca2570b454f75f3395e003b031724a22b2beeea
Carlos Marban <152007178+zencare4k@users.noreply.github.com>
2024-10-23 14:41:11 +0200

    xd

--- 1T/Opcionales/ejercicio1/ejercicio1/src/Tabla.jsx
+++ 1T/Opcionales/ejercicio1/ejercicio1/src/Tabla.jsx
@@ -15,6 +15,7 @@ export default function DiscordTable() {
                     </tr>
                 </thead>
                 <tbody>
+                    
                 </tbody>
             </table>
         </div>
