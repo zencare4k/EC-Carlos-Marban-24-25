2f8f2cb43d1b136aa51d6d078b0a433ab8cccf8d
z4k23 <122319845+z4k23@users.noreply.github.com>
2025-01-18 00:30:41 +0100

    xd

--- 1T/Sprint_2.1/sprint_2.1/src/App.js
+++ 1T/Sprint_2.1/sprint_2.1/src/App.js
@@ -1,17 +1,21 @@
-import './App.css';
-import Footer from './components/Layout/Footer'
-import Header from './components/Layout/NavBar'
-function App() {
-  return (
-    <div>
-      <header>
-      <Header/>
-      </header>
-      <footer>
-      <Footer/>
-      </footer>
-    </div>
-  );
-}
+  import './App.css';
+  import Footer from './components/Layout/Footer'
+  import Header from './components/Layout/NavBar'
+  import Herosection from './components/Home/HeroSection';
+  function App() {
+    return (
+      <div>
+        <header>
+        <Header/>
+        </header>
+        <main>
+        <Herosection/>
+        </main>
+        <footer>
+        <Footer/>
+        </footer>
+      </div>
+    );
+  }
 
-export default App;
+  export default App;
